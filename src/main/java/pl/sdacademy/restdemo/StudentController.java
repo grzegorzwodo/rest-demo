fed7c1aa1196a595e12c4a7320a61e84026398a7
Grzegorz <grzegorz.wodo@gmail.com>
2019-02-15 20:01:13 +0100

    Add comments

--- src/main/java/pl/sdacademy/restdemo/StudentController.java
+++ src/main/java/pl/sdacademy/restdemo/StudentController.java
@@ -1,15 +1,25 @@
 package pl.sdacademy.restdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class StudentController {
 
-    @GetMapping("/")
+    @GetMapping("/students")
     public Student getStudent() {
+        //TODO get student from file student.json
         Student student = new Student("test", "test2");
         return student;
+    }
+
+    @PostMapping("/students")
+    public void createStudent(@RequestBody Student student) throws IOException {
+        //TODO save student to file student.json
     }
 
 }
